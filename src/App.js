75b25d7573ed804af35ff7a1ae82d6a2434e5b8e
Miran Ahmad <222miran222@gmail.com>
2023-07-09 12:31:57 +0530

    sidebar start

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Sidebar from "./Sidebar";
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <h1>Miran</h1>
+      {/* Sidebar */}
+      <Sidebar />
+      {/* Feed */}
+
+      {/* Widgets */}
+    </div>
+  );
+}
+
+export default App;
